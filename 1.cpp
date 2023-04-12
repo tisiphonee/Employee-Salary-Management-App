ab80be538ef34b91c49c49225a411bcfedcecf99
tisiphonee <mohsenino.mh@gmail.com>
2023-04-12 10:42:25 +0330

    Added 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+class Team
+{
+
+public:
+    Team(int team_id, int team_head_id, vector<int> member_ids, int binus_min_working_hours, int binus_working_hours_max_variancse);
+    int get_team_id() { return team_id; }
+    int get_team_head_id() { return team_head_id; }
+    vector<int> get_member_ids() { return member_ids; }
+    int get_binus_min_working_hours() { return binus_min_working_hours; }
+    int get_binus_working_hours_max_variancse() { binus_working_hours_max_variancse; }
+
+private:
+    int team_id;
+    int team_head_id;
+    vector<int> member_ids;
+    int binus_min_working_hours;
+    int binus_working_hours_max_variancse;
+};
+
+Team::Team(int team_id, int team_head_id, vector<int> member_ids, int binus_min_working_hours, int binus_working_hours_max_variancse)
+{
+    read_teams_file();
+}
+
+vector<Team *> read_teams_file()
+{
+    ifstream file("teams.csv");
+    int position_in_string = 0;
+    int team_id, team_head_id, binus_min_working_hours, binus_working_hours_max_variancse;
+    vector<int> member_ids;
+    string line;
+
+    vector<Team *> teams;
+    getline(file, line);
+
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        string field;
+
+        getline(ss, field, ',');
+        team_id = stoi(field);
+
+        getline(ss, field, ',');
+        team_head_id = stoi(field);
+
+        getline(ss, field, ',');
+        while (position_in_string != field.size())
+        {
+            int $_position;
+            string member_id;
+            $_position = field.find('$');
+            member_id = field.substr(position_in_string, $_position);
+            member_ids.push_back(stoi(member_id));
+        }
+
+        getline(ss, field, ',');
+        binus_min_working_hours = stoi(field);
+
+        getline(ss, field, ',');
+        binus_working_hours_max_variancse = stoi(field);
+        teams.push_back(new Team(team_id, team_head_id, member_ids, binus_min_working_hours, binus_working_hours_max_variancse));
+    }
+
+    file.close();
+    return teams;
+}
+
+int main()
+{
+}
+
+int main()
+{
+    read_teams_file();
+}
